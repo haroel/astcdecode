cd7461defd25cb45dd4019fa26558dc40c0bc178
howe <ihowe@outlook.com>
2023-04-04 15:11:29 +0800

    Update astcdec.h

--- astcdec.h
+++ astcdec.h
@@ -1,7 +1,7 @@
 #ifndef _TCUASTCUTIL_HPP
 #define _TCUASTCUTIL_HPP
 
-#include <vector>
+#include <stdint.h>
 
 namespace basisu
 {
@@ -11,8 +11,7 @@ namespace basisu
 // Unpacks a single ASTC block to pDst
 // If isSRGB is true, the spec requires the decoder to scale the LDR 8-bit endpoints to 16-bit before interpolation slightly differently,
 // which will lead to different outputs. So be sure to set it correctly (ideally it should match whatever the encoder did).
-        bool decompress(uint8_t* pDst, const uint8_t* data, bool isSRGB, int blockWidth, int blockHeight);
-
+        uint32_t transferToRGBA( uint8_t *astc_file_data, uint32_t astc_file_size, unsigned char **outRGBA ,uint32_t *outWidth, uint32_t *outHeight );
     } // astc
 } // basisu
 
